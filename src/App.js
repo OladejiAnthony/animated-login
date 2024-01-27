3204ac9a06e101358b4965c021518574b9d14635
oladeji anthony <oladejianthony98@gmail.com>
2024-01-27 09:34:02 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import AuthContainer from "./components/auth/AuthContainer";
+import PwdLogin from "./components/pwdToggle/PwdToggle";
 
 function App() {
   return (
     <div>
       <AuthContainer />
+      <PwdLogin />
     </div>
   );
 }
